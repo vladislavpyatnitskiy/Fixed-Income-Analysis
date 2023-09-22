fcf5b0c05bf614f223715afafcd2732a0c0810cd
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-22 21:09:59 +0500

    Update IRR.R
    Optimised IRR function:
    
    • Integrated NPV generating function into IRR function so no need to calculate NPV separately

--- Accounting measures/IRR.R
+++ Accounting measures/IRR.R
@@ -1,16 +1,11 @@
 # Define Cash Flow
 irr_cash_flows1 <- c(-800, 300, 300, 300, 150)
 
-# Function for NPV
-NPV <- function(C, r) {
-  sum(C / (1 + r) ^ (seq(along = C) - 1))
-}
-
 # Function for IRR
-IRR <- function(C) {
-  uniroot(NPV, c(0, 1), C = C)$root
+IRR <- function(C, r) {
+  uniroot(function(C, r) (sum(C / (1 + r) ^ (seq(along = C) - 1))),
+          c(0, 1), C = C)$root
 }
 
 # Test
-NPV(C = irr_cash_flows1, r  = 0.1)
-IRR(C = irr_cash_flows1)
+IRR(C = irr_cash_flows1, r  = 0.1)
