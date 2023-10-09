3c2103a512af57041ad27ff86d05ed60c52185bc
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-09 21:12:35 +0500

    Update IRR & NPV graph.R
    Optimised script:
    
    • Incorporated options to configure colour, width and height of breakeven line
    • Added option to choose focus of line according to x-axis
    • Reduced excessive script in data frame part

--- Accounting measures/IRR & NPV graph.R
+++ Accounting measures/IRR & NPV graph.R
@@ -1,42 +1,34 @@
-# Cash Flow
+# Project Cash Flow
 cash_flows2 <- c(-800, 300, 300, 300, 150)
 
-# Function to plot graph
-npv_irr_graph <- function(x){
+# Plot relationship between IRR and NPV
+npv_irr_graph <- function(x, xlim = NULL, h = 0, col, lwd = 3){
   
   # Make a vector with irr's and make it matrix
   irr_matrix <- as.matrix(seq(0, 1, by  = 0.01))
   
-  # Create a variable to store NPV values
+  # Create 
   values_for_npv_graph <- NULL
   
   # For each irr values
   for (v in 1:nrow(irr_matrix)){
     
-    # Calculate NPV value
-    graph_value <- sum(x / (1 + irr_matrix[v]) ^ (seq(along = x) - 1))
-    
     # Put value in vector
-    values_for_npv_graph <- c(values_for_npv_graph, graph_value)
-  }
-  # Make vector with NPV values a matrix
-  values_for_npv_graph <- as.matrix(values_for_npv_graph)
+    values_for_npv_graph <- rbind(values_for_npv_graph,
+                                  sum(x / (1 + irr_matrix[v])
+                                      ^ (seq(along = x) - 1))) }
   
   # Join NPV and IRR into one data frame
-  final_values_for_npv_graph <- data.frame(irr_matrix,
-                                           values_for_npv_graph)
+  final_values_for_npv_graph <- data.frame(irr_matrix, values_for_npv_graph)
+  
   # Create column names 
   colnames(final_values_for_npv_graph) <- c("IRR", "NPV")
   
   # Plot 
-  plot(x = final_values_for_npv_graph,
-       type = "l",
-       xlim = c(0.00, 0.2))
+  plot(x = final_values_for_npv_graph, type = "l", xlim = xlim)
   
   # Make break even line more visible
-  abline(h = 0,
-         col = "red",
-         lwd = 2)
+  abline(h = h, col = col, lwd = lwd)
 }
 # Test
-npv_irr_graph(cash_flows2)
+npv_irr_graph(cash_flows2, xlim = c(0, 0.2), h = 0, col = "red", lwd = 3)
