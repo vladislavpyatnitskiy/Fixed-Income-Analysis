61ca32b07711b7f12b40ca15120813c43a537399
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-22 21:44:48 +0500

    Update IRR.R
    Reduced excessive lines

--- Accounting measures/IRR.R
+++ Accounting measures/IRR.R
@@ -4,8 +4,6 @@ irr_cash_flows1 <- c(-800, 300, 300, 300, 150)
 # Function for IRR
 IRR <- function(C, r) {
   uniroot(function(C, r) (sum(C / (1 + r) ^ (seq(along = C) - 1))),
-          c(0, 1), C = C)$root
-}
-
+          c(0, 1), C = C)$root }
 # Test
 IRR(C = irr_cash_flows1, r  = 0.1)
