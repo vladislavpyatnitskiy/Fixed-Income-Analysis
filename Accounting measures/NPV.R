3b1874adb32033be597fdf072c62c18f777965ba
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-22 21:42:17 +0500

    Update NPV.R
    Reduced excessive lines

--- Accounting measures/NPV.R
+++ Accounting measures/NPV.R
@@ -5,11 +5,7 @@ cash_flow <- c(-1000, 250, 300, 360, 432)
 rate_of_return <- 0.05
 
 # NPV function
-NPV <- function(C, r) {
-
-  # Sum of cash flow adjusted by rate of return
-  sum(C / (1 + r) ^ (seq(along = C) - 1))
-}
+NPV <- function(C, r) { sum(C / (1 + r) ^ (seq(along = C) - 1)) }
 
 # Test
 NPV(cash_flow, rate_of_return)
