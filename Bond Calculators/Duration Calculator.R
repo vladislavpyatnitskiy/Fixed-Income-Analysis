ac3c583d662008f31c2e37b54558b96dca31ed02
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-22 21:07:50 +0500

    Update Duration Calculator.R
    Optimised script:
    
    • Substituted some loops with sequences
    • Created variables to substitute some operations
    • Changed variable names to reduce lines
    • Combined operations

--- Bond Calculators/Duration Calculator.R
+++ Bond Calculators/Duration Calculator.R
@@ -4,12 +4,17 @@ bond_duration <- function(bond_principle,
                           bond_interest_rate,
                           bond_year_to_mat,
                           bond_sensitivity = 1){
+  # Maturity for loops
+  ytm <- bond_year_to_mat - 1
+  
+  # Rate for nominator
+  rt <- 1 + bond_interest_rate
+  
   # Coupon calculation
   coupon_part <- bond_coupon_rate * bond_principle
   
   # Calculate part for principle
-  principle_part <- ((bond_principle + coupon_part) / 
-                       ((1 + bond_interest_rate)^bond_year_to_mat))
+  principle_part <- (bond_principle + coupon_part) / (rt ^ bond_year_to_mat)
   
   # Multiply adjusted principle to number of YtM
   pv_principle <- principle_part * bond_year_to_mat
@@ -18,64 +23,29 @@ bond_duration <- function(bond_principle,
   pv_sum <- NULL
   
   # Calculate PV of coupons
-  for (n in 1:(bond_year_to_mat-1)){
-    # for each flow of cash
-    duration_pv <- coupon_part / ((1 + bond_interest_rate) ^ n)
-    
-    # Put each flow of cash in list
-    pv_sum <- cbind(pv_sum, duration_pv)
-  }
+  for (n in 1:ytm){ pv_sum <- cbind(pv_sum, coupon_part / rt ^ n) }
   
-  # Set list for adjusted coupons values to be added in denominator
-  values_for_denominator <- 0
-  
-  # Coupon part for denominator
-  for (n in 1:(bond_year_to_mat - 1)){
-    values_for_denominator <- values_for_denominator + pv_sum[n]
-  }
-  
-  # Main denominator for duration
-  final_dur_denominator <- principle_part + 
-    values_for_denominator
+  # Add adjusted coupons sum to principle part to find out main denominator
+  fin_dur_denom <- principle_part + sum(pv_sum[seq(ytm)])
   
   # Set up new list to contain
-  list_of_payments <- NULL
+  pmnts_list <- NULL
   
   # Coupon part for numerator
-  for (n in 1:(bond_year_to_mat - 1)) {
-    payments_sum <- n * pv_sum[n]
-    
-    list_of_payments <- cbind(list_of_payments, payments_sum)
-  }
+  for (n in 1:ytm){ pmnts_list <- cbind(pmnts_list, n * pv_sum[n]) }
   
-  # Define variable to contain adjusted coupons
-  sum_of_duration_payments <- 0
-  
-  # Sum adjusted coupons
-  for (n in 1:(bond_year_to_mat - 1)){
-    sum_of_duration_payments <- sum_of_duration_payments + 
-      list_of_payments[n]
-  }
-  
-  # Sum cash flows to principle
-  duration_value <- (sum_of_duration_payments + pv_principle) /
-    final_dur_denominator
-  
-  # Round value for modified duration
-  modified_duration <- round(duration_value /
-    (1 + (bond_interest_rate - bond_sensitivity * 0.01)), 2)
-  
-  # Round value for duration
-  duration_value <- round(duration_value, 3)
+  # Sum cash flows to principle and round value for modified duration
+  mod.dur <- round(((sum(pmnts_list[seq(ytm)]) + pv_principle)/fin_dur_denom)/
+                     (1 +(bond_interest_rate - bond_sensitivity * 0.01)), 2)
   
   # Put values into list
-  bond_list <- cbind(duration_value, modified_duration)
+  bond.list <- cbind(round(duration_value, 3), mod.dur)
   
   # Set column names 
-  colnames(bond_list) <- c("Duration", "Modified Duration")
+  colnames(bond.list) <- c("Duration", "Modified Duration")
   
   # Display sentence
-  return(bond_list)
+  return(bond.list)
 }
 # Test
 bond_duration(1000, 0.1, 0.05, 3)
