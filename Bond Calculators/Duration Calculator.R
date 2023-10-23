c4cab59d3cb468947b41a99cbea203a0ed07a226
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-23 17:52:21 +0500

    Update Duration Calculator.R
    Renamed names of variables

--- Bond Calculators/Duration Calculator.R
+++ Bond Calculators/Duration Calculator.R
@@ -1,45 +1,41 @@
-# Function to calculate both Duration and modified duration of bond
-bond_duration <- function(bond_principle,
-                          bond_coupon_rate,
-                          bond_interest_rate,
-                          bond_year_to_mat,
-                          bond_sensitivity = 1){
+# Duration and Modified Duration
+Duration <- function(P, C, r, ytm, s = 1){
+  
   # Maturity for loops
-  ytm <- bond_year_to_mat - 1
+  ytm.a <- ytm - 1
   
   # Rate for nominator
-  rt <- 1 + bond_interest_rate
+  rt <- 1 + r
   
   # Coupon calculation
-  coupon_part <- bond_coupon_rate * bond_principle
+  C.part <- C * P
   
   # Calculate part for principle
-  principle_part <- (bond_principle + coupon_part) / (rt ^ bond_year_to_mat)
+  P.part <- (P + C.part) / (rt ^ ytm)
   
   # Multiply adjusted principle to number of YtM
-  pv_principle <- principle_part * bond_year_to_mat
+  PV.P <- P.part * ytm
   
   # Set up duration sum
-  pv_sum <- NULL
+  PV.sum <- NULL
   
   # Calculate PV of coupons
-  for (n in 1:ytm){ pv_sum <- cbind(pv_sum, coupon_part / rt ^ n) }
-  
-  # Add adjusted coupons sum to principle part to find out main denominator
-  fin_dur_denom <- principle_part + sum(pv_sum[seq(ytm)])
+  for (n in 1:ytm.a){ PV.sum <- cbind(PV.sum, C.part / rt ^ n) }
   
   # Set up new list to contain
-  pmnts_list <- NULL
+  payments <- NULL
   
   # Coupon part for numerator
-  for (n in 1:ytm){ pmnts_list <- cbind(pmnts_list, n * pv_sum[n]) }
+  for (n in 1:ytm.a){ payments <- cbind(payments, n * PV.sum[n]) }
   
-  # Sum cash flows to principle and round value for modified duration
-  mod.dur <- round(((sum(pmnts_list[seq(ytm)]) + pv_principle)/fin_dur_denom)/
-                     (1 +(bond_interest_rate - bond_sensitivity * 0.01)), 2)
+  # Duration
+  D <- (sum(payments[seq(ytm.a)]) + PV.P) / (P.part + sum(PV.sum[seq(ytm.a)]))
+  
+  # Modified duration
+  MD <- D / (1 + (r - s * 0.01))
   
   # Put values into list
-  bond.list <- cbind(round(duration_value, 3), mod.dur)
+  bond.list <- cbind(round(D, 3), round(MD, 2))
   
   # Set column names 
   colnames(bond.list) <- c("Duration", "Modified Duration")
@@ -48,4 +44,4 @@ bond_duration <- function(bond_principle,
   return(bond.list)
 }
 # Test
-bond_duration(1000, 0.1, 0.05, 3)
+Duration(1000, 0.1, 0.05, 3)
