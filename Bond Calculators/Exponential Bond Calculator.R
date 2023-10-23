e2a2602eb9b88de7c3b3b73450670897dfab4a6f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-23 16:50:20 +0500

    Update Exponential Bond Calculator.R
    Renamed names of variables

--- Bond Calculators/Exponential Bond Calculator.R
+++ Bond Calculators/Exponential Bond Calculator.R
@@ -1,22 +1,17 @@
 # Exponential Bond Calculator
-exp_bond_calculator <- function(bond_principle,
-                                bond_coupon_rate,
-                                bond_interest_rate,
-                                bond_year_to_mat,
-                                n_an_b = 1){
+B.calculator.exp <- function(P, C, r, ytm, f = 1){
   
-  # Calculate Principal part
-  exp_principal <- bond_principle * exp(-bond_interest_rate * bond_year_to_mat)
+  # Principal part
+  P.exp <- P * exp(-r * ytm)
   
-  # Define new variable for number of coupon repayments
-  repay <- bond_year_to_mat * n_an_b
+  # Number of coupon repayments
+  repay <- ytm * f
   
-  # Set up variable name for sum of coupons
-  exp_coupon <- sum(((bond_coupon_rate * bond_principle) / n_an_b) *
-                      exp(-bond_interest_rate * (seq(repay) / repay)))
+  # Sum of coupons
+  C.exp <- sum(((C * P) / f) * exp(-r * (seq(repay) / repay)))
   
-  # Calculate Bond Value and display it
-  return(exp_principal + exp_coupon)
+  # Bond Value
+  return(P.exp + C.exp)
 }
 # Test
-exp_bond_calculator(100, 0.06, 0.0676, 2, 2)
+B.calculator.exp(100, 0.06, 0.0676, 2, 2)
