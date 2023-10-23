15af6fcefc81fb51b8a321484c00a674a6fc2724
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-23 16:30:04 +0500

    Update Bond Params Table.R
    Optimised: removed excessive scripts & renamed variables

--- Bond Calculators/Bond Params Table.R
+++ Bond Calculators/Bond Params Table.R
@@ -1,151 +1,85 @@
 # Create Table with Bond Parameters values
-bond_param_table <- function(bond_principle,
-                           bond_coupon_rate,
-                           bond_interest_rate,
-                           bond_year_to_mat,
-                           n_an_b = 1,
-                           bond_sensitivity = 1){
+B.table <- function(P, C, r, ytm, f = 1, s = 1){
+  
+  # Maturity for loops
+  ytm.a <- ytm - 1
+  
+  # Rate for nominator
+  rt <- 1 + r
   
   ### Duration Part ###
   
   # Coupon calculation
-  coupon_part <- bond_coupon_rate * bond_principle
+  C.part <- (C * P) / f
   
   # Calculate part for principle
-  principle_part <- ((bond_principle + coupon_part) / 
-                       ((1 + bond_interest_rate)^bond_year_to_mat))
+  P.part <- (P + C.part) / rt ^ ytm
   
   # Multiply adjusted principle to number of YtM
-  pv_principle <- principle_part * bond_year_to_mat
+  PV.P <- P.part * ytm
   
   # Set up duration sum
-  pv_sum <- NULL
+  PV.sum <- NULL
   
   # Calculate PV of coupons
-  for (n in 1:(bond_year_to_mat-1)){
-    # for each flow of cash
-    duration_pv <- coupon_part / ((1 + bond_interest_rate) ^ n)
-    
-    # Put each flow of cash in list
-    pv_sum <- cbind(pv_sum, duration_pv)
-  }
-  
-  # Set list for adjusted coupons values to be added in denominator
-  values_for_denominator <- 0
-  
-  # Coupon part for denominator
-  for (n in 1:(bond_year_to_mat - 1)){
-    values_for_denominator <- values_for_denominator + pv_sum[n]
-  }
-  
-  # Main denominator for duration
-  final_dur_denominator <- principle_part + 
-    values_for_denominator
+  for (n in 1:ytm.a){ PV.sum <- cbind(PV.sum, C.part / rt ^ n) }
   
   # Set up new list to contain
-  list_of_payments <- NULL
+  payments <- NULL
   
   # Coupon part for numerator
-  for (n in 1:(bond_year_to_mat - 1)) {
-    payments_sum <- n * pv_sum[n]
-    
-    list_of_payments <- cbind(list_of_payments, payments_sum)
-  }
+  for (n in 1:ytm.a) { payments <- cbind(payments, n * PV.sum[n]) }
   
-  # Define variable to contain adjusted coupons
-  sum_of_duration_payments <- 0
-  
-  # Sum adjusted coupons
-  for (n in 1:(bond_year_to_mat - 1)){
-    sum_of_duration_payments <- sum_of_duration_payments + 
-      list_of_payments[n]
-  }
-  
-  # Sum cash flows to principle
-  duration_value <- (sum_of_duration_payments + pv_principle) /
-    final_dur_denominator
+  # Add adjusted coupons sum to principle part
+  D <- (sum(payments[seq(ytm.a)]) + PV.P) / (P.part + sum(PV.sum[seq(ytm.a)]))
   
   ### Modified Duration Part ###
   
-  # Round value for modified duration
-  modified_duration <- round(duration_value /
-                               (1 +
-                                  (bond_interest_rate -
-                                     bond_sensitivity * 0.01)),
-                             2)
+  # Bond Price when yield goes down by 1%
+  p.change <- r - s * 0.01
   
-  # Round value for duration
-  duration_value <- round(duration_value, 3)
+  # Bond Price when yield goes up by 1%
+  n.change <- r + s * 0.01
+  
+  # Round value for modified duration
+  MD <- round(D / (1 + p.change), 2)
   
   ### Convexity Part ###
   
-  # Bond Price when yield goes down by 1%
-  positive_b_chng <- bond_interest_rate - bond_sensitivity * 0.01
-  
-  # Bond Price when yield goes up by 1%
-  negative_b_chng <- bond_interest_rate + bond_sensitivity * 0.01
-  
   # Put all interest rate values into one vector
-  bond_i_r <- c(bond_interest_rate,
-                positive_b_chng,
-                negative_b_chng)
+  r.Cy <- c(r, p.change, n.change)
   
   # Create an empty list to contain all price values
-  bond_price_list <- NULL
+  v.Cy <- NULL
   
-  # For each bond price
-  for (n in 1:3){
+  # Calculate all 3 prices; product of repayment number and rate
+  for (n in 1:3){ repay <- f / r.Cy[n]
     
-    # calculate coupon part
-    coupon_part <- (bond_coupon_rate * bond_principle) / n_an_b
+    # Product of maturity and repayment number
+    mat_x_rep <- (1 + 1/repay) ^ (ytm * f)
     
     # calculate interest rate part
-    bond_rate_part <- ((n_an_b/(bond_i_r[n])) -
-                         n_an_b/((bond_i_r[n]) *
-                                   (1 + (bond_i_r[n])/n_an_b) ^
-                                   bond_year_to_mat * n_an_b))
-    # Calculate principle part
-    principle_part <- bond_principle / ((1 + (bond_i_r[n])/n_an_b) ^
-                                          bond_year_to_mat * n_an_b)
-    # Calculate price of bond
-    price_of_the_bond = coupon_part * bond_rate_part + principle_part
+    r.part <- repay - f / (r.Cy[n] * mat_x_rep)
     
-    # Add result to list
-    bond_price_list <- cbind(bond_price_list,
-                             price_of_the_bond)
+    # Calculate principle part
+    P.part <- P / mat_x_rep
+    
+    # Calculate price of bond and add result to list
+    v.Cy <- cbind(v.Cy, C.part * r.part + P.part)
   }
-  # Current price of bond
-  bond_now <- bond_price_list[1]
-  
-  # Bond price when yield goes down
-  bond_up <- bond_price_list[2]
-  
-  # Bond price when yield goes up
-  bond_down <- bond_price_list[3]
-  
   # Calculate Convexity
-  convexity_result <- round((bond_up + bond_down - 2 * bond_now) / 
-    (bond_now * ((bond_sensitivity * 0.01) ^ 2)), 2)
+  Cy <- (v.Cy[2] + v.Cy[3] - 2 * v.Cy[1]) / (v.Cy[1] * (s * 0.01) ^ 2)
   
-  ### End of Calculations ### 
+  # End of Calculations #
   
   # Put values into list
-  bond_list <- cbind(duration_value,
-                     modified_duration,
-                     convexity_result)
+  l.ratios <- cbind(round(D, 3), MD, round(Cy, 2))
   
   # Set column names 
-  colnames(bond_list) <- c("Duration",
-                           "Modified Duration",
-                           "Convexity")
+  colnames(l.ratios) <- c("Duration", "Modified Duration", "Convexity")
   
   # Display sentence
-  return(bond_list)
+  return(l.ratios)
 }
 # Test
-bond_param_table(bond_principle = 1000,
-                 bond_coupon_rate = 0.1,
-                 bond_interest_rate = 0.05,
-                 bond_year_to_mat = 3,
-                 n_an_b = 1,
-                 bond_sensitivity = 1)
+B.table(P = 1000, C = 0.1, r = 0.05, ytm = 3, f = 1, s = 1)
