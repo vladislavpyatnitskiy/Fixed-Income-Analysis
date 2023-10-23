5c9e24e354a79b7ea21c9e6ce4ccf2bafd7e3205
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-23 20:41:41 +0500

    Update Exponential Bond Calculator.R
    Optimised script by reducing excessive steps and making script one line

--- Bond Calculators/Exponential Bond Calculator.R
+++ Bond Calculators/Exponential Bond Calculator.R
@@ -1,17 +1,8 @@
 # Exponential Bond Calculator
 B.calculator.exp <- function(P, C, r, ytm, f = 1){
   
-  # Principal part
-  P.exp <- P * exp(-r * ytm)
-  
-  # Number of coupon repayments
-  repay <- ytm * f
-  
-  # Sum of coupons
-  C.exp <- sum(((C * P) / f) * exp(-r * (seq(repay) / repay)))
-  
   # Bond Value
-  return(P.exp + C.exp)
+  P * exp(-r * ytm) + sum(((C * P) / f) * exp(-r * (seq(ytm * f) / (ytm * f))))
 }
 # Test
 B.calculator.exp(100, 0.06, 0.0676, 2, 2)
