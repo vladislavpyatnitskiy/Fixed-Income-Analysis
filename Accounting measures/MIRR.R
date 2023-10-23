3094ff1cc8c2d69e0c04eb504d4bc1fbc4e9eb27
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-23 18:27:30 +0500

    Update MIRR.R
    renamed variables

--- Accounting measures/MIRR.R
+++ Accounting measures/MIRR.R
@@ -4,17 +4,17 @@ cash_flow_for_mirr1 <- c(200, 400, 350, 300)
 # Second Project
 cash_flow_for_mirr2 <- c(300, 300, 300, 300) # y = 0.12 cf_0 = 800
 
-# function for MIRR
-mirr_cash_flow <- function(x, y = 0.1, cf_0 = 1000){
+# MIRR
+MIRR <- function(C, r = 0.1, I = 1000){
   
-  # Calculate MIRR
-  mirr <- (sum(x*((1 + y)^(length(x) - seq(x))))/cf_0)^(1/length(x)) - 1
+  # MIRR
+  mirr <- (sum(C * ((1 + r) ^ (length(C) - seq(C)))) / I) ^ (1 / length(C)) - 1
   
-  # Recommendation whether to start a project or not
-  if (mirr > y){ print(sprintf("MIRR is %s%% > cost of capital. Start project",
-                                      round(mirr, 4) * 100)) } else {
-    print(sprintf("MIRR is %s%% < cost of capital. Don't start",
-                  round(mirr, 4) * 100)) }
+  # Recommendation
+  if (mirr > r){ print(sprintf("MIRR is %s%% > cost of capital. Start project",
+                               round(mirr, 4) * 100)) 
+  } else { print(sprintf("MIRR is %s%% < cost of capital. Don't start",
+                           round(mirr, 4) * 100)) }
 }
 # Test
-mirr_cash_flow(x = cash_flow_for_mirr2, y = 0.12, cf_0 = 800)
+MIRR(C = cash_flow_for_mirr2, r = 0.12, I = 800)
