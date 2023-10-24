67f0c52e3e23d574a38bbefe8b0833e6856121af
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-24 14:48:08 +0500

    Update Bond Params Table.R
    Optimised script:
    
    • Combined 2 for loops into 1
    • Substituted values for years to maturity 
    • Changed variable name in convexity part
    • Put comments right where appropriate

--- Bond Calculators/Bond Params Table.R
+++ Bond Calculators/Bond Params Table.R
@@ -1,45 +1,33 @@
-# Create Table with Bond Parameters values
+# Create Table with Bond Parameters
 B.table <- function(P, C, r, ytm, f = 1, s = 1){
   
-  # Maturity for loops
-  ytm.a <- ytm - 1
-
-  ### Duration Part ###
+  # Duration #
+  P.part <- (P * (1 + C / f)) / (1 + r) ^ ytm # Calculate part for principle
   
-  # Calculate part for principle
-  P.part <- (P * (1 + C / f)) / (1 + r) ^ ytm
-  
-  # Set up duration sum
+  # Duration sum and payments
   PV.sum <- NULL
-  
-  # Calculate PV of coupons
-  for (n in 1:ytm.a){ PV.sum <- cbind(PV.sum, ((C * P) / f) / (1 + r) ^ n) }
-  
-  # Set up new list to contain
   payments <- NULL
   
-  # Coupon part for numerator
-  for (n in 1:ytm.a) { payments <- cbind(payments, n * PV.sum[n]) }
+  # Calculate PV of coupons
+  for (n in 1:(ytm-1)){ PV.sum <- cbind(PV.sum, ((C * P ) / f ) / (1 + r ) ^ n) 
+    
+    payments <- cbind(payments, n * PV.sum[n]) } # Coupon part for numerator
   
   # Duration
-  D <- (sum(payments[seq(ytm.a)])+P.part*ytm)/(P.part+sum(PV.sum[seq(ytm.a)]))
+  D <- (sum(payments[seq(ytm-1)])+P.part*ytm)/(P.part+sum(PV.sum[seq(ytm-1)]))
   
-  ### Convexity Part ###
+  # Convexity #
   
   # Put all interest rate values into one vector
   r.Cy <- c(r, r - s * 0.01, r + s * 0.01)
   
-  # Create an empty list to contain all price values
-  v.Cy <- NULL
+  v.Cy <- NULL # Create an empty list to contain all price values
   
   # Calculate all 3 prices; Product of maturity and repayment number
-  for (n in 1:3){ mat_x_rep <- (1 + 1 / (f / r.Cy[n])) ^ (ytm * f)
-    
-    # Calculate interest rate part
-    r.part <- f / r.Cy[n] * (1 - 1 / mat_x_rep)
-    
-    # Calculate price of bond and add result to list
-    v.Cy <- cbind(v.Cy, ((C * P) / f) * r.part + P / mat_x_rep) }
+  for (n in 1:3){ d <- (1 + 1 / (f / r.Cy[n])) ^ (ytm * f)
+  
+  # Calculate price of bond and add result to list
+  v.Cy <- cbind(v.Cy, ((C * P) / f) * (f / r.Cy[n] * (1 - 1 / d)) + P / d) }
   
   # Calculate Convexity
   Cy <- (v.Cy[2] + v.Cy[3] - 2 * v.Cy[1]) / (v.Cy[1] * (s * 0.01) ^ 2)
