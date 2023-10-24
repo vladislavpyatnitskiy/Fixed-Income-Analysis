ecf824bf0071bf28108669f72abc00399ee4f199
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-24 15:00:49 +0500

    Update Bond Calculator.R
    Optimised script:
    
    • Made standard valuation part one line
    • Eliminated excess variable in the equation

--- Bond Calculators/Bond Calculator.R
+++ Bond Calculators/Bond Calculator.R
@@ -3,11 +3,8 @@ B.calculator <- function(P, C, r, ytm, f = 1, exp = F){
   
   # Exponential Valuation
   if (isTRUE(exp)){ P*exp(-r*ytm) + sum(((C*P)/f)*exp(-r*(seq(ytm*f)/(ytm*f))))
-    
-    # Standard Valuation
-    } else { d <- 1 / (1 + (1 / (f / r))) ^ (ytm * f)
-  
-    P * (C / f * (f / r * (1 - d)) + d) }
+  # Standard Valuation
+  } else { P * (C*(1/r*(1-1/(1+(1/(f/r)))^(ytm*f))) + 1/(1+(1/(f/r)))^(ytm*f))}
 }
 # Test it
 B.calculator(1000, 0.08, 0.06, 30, 2, F)
