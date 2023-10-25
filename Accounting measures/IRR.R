7163809943b8f4b075dcf135264576af1ea4b554
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-25 10:31:52 +0500

    Update IRR.R
    • Made function script one line
    • Put cash flow and rate into function

--- Accounting measures/IRR.R
+++ Accounting measures/IRR.R
@@ -1,9 +1,6 @@
-# Define Cash Flow
-irr_cash_flows1 <- c(-800, 300, 300, 300, 150)
-
 # Function for IRR
 IRR <- function(C, r) {
-  uniroot(function(C, r) (sum(C / (1 + r) ^ (seq(along = C) - 1))),
-          c(0, 1), C = C)$root }
+  uniroot(function(C, r) (sum(C/(1+r)^(seq(along = C)-1))), c(0,1), C=C)$root }
+
 # Test
-IRR(C = irr_cash_flows1, r  = 0.1)
+IRR(C = c(-800, 300, 300, 300, 150), r = 0.1)
