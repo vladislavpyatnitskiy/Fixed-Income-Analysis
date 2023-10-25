b386e53731d1ef40cb9bd79898243342e0c66ca9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-25 14:40:32 +0500

    Update Discounted Payback Rule.R
    Optimised script:
    
    • Reduced step of adding cash flow to payback,  so there are just 2 steps
    • Shortened comments
    • Put zero values inside set variables

--- Accounting measures/Discounted Payback Rule.R
+++ Accounting measures/Discounted Payback Rule.R
@@ -1,21 +1,15 @@
 # Discounted Payback
-DPR <- function(C, r, I){
-  
-  DP <- 0 # Define Payback Sum
-  period <- 0 # and Payback Period
+DPR <- function(C, r, I, DP = 0, period = 0){
   
   # When Payback Sum < Initial Value add Cash Flow for each year
-  while (DP <= I){ for (n in 1:length(C)){ DCF <- C[n] / (1 + r) ^ n
+  while (DP <= I){ for (n in 1:length(C)){ DP <- DP + C[n] / (1 + r) ^ n
   
-    DP <- DP + DCF # Payback Sum
+  # When Payback Sum > Initial Value, calculate Payback Period
+  if (DP > I){ period <- (I - DP) / (C[n] / (1 + r) ^ n) + n
   
-  # When Payback Sum > Initial Value, calculate Payback Period and stop
-  if (DP > I){ period <- (I - DP) / DCF + n
+  break } } } # stop
   
-    break } } }
-    
-  # Put number into sentence and display value
-  sprintf("The payback period is %s years", round(period, 2))
+  sprintf("Payback period is %s years", round(period, 2)) # Display value
 }
 # Test
 DPR(C = c(100, 100, 100, 100), r = 0.1, I = 200)
