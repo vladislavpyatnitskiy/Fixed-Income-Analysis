fd6dfd63c2a31414f976034f424c4aeb6ed3dd54
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-25 10:26:40 +0500

    Update NPV.R
    Reduced lines with cash flow and rate and integrated them into function

--- Accounting measures/NPV.R
+++ Accounting measures/NPV.R
@@ -1,11 +1,5 @@
-# Cash Flow
-cash_flow <- c(-1000, 250, 300, 360, 432)
-
-# Rate of Return
-rate_of_return <- 0.05
-
 # NPV function
 NPV <- function(C, r) { sum(C / (1 + r) ^ (seq(along = C) - 1)) }
 
 # Test
-NPV(cash_flow, rate_of_return)
+NPV(C = c(-1000, 250, 300, 360, 432), r = 0.05)
