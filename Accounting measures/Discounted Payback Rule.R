c9525e6714a1e9c527daf03f2aad7624f80614c7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-25 10:21:55 +0500

    Update Discounted Payback Rule.R
    Optimised script by rewriting equations and comments

--- Accounting measures/Discounted Payback Rule.R
+++ Accounting measures/Discounted Payback Rule.R
@@ -1,27 +1,21 @@
-# Cash flows
-cash_flow_for_dpr <- c(100, 100, 100, 100)
-
 # Discounted Payback
-DPR <- function(C, r = 0.1, I = 200){
+DPR <- function(C, r, I){
   
-  # Define sum of paybacks and value to store period for paybacks
-  DP <- 0
-  period <- 0
+  DP <- 0 # Define Payback Sum
+  period <- 0 # and Payback Period
   
-  # Until sum of payback is less than initial value calculate payback
+  # When Payback Sum < Initial Value add Cash Flow for each year
   while (DP <= I){ for (n in 1:length(C)){ DCF <- C[n] / (1 + r) ^ n
-    
-    # Sum paybacks
-    DP <- DP + DCF
-    
-    # When sum of payback is more than initial value, stop
-    if (DP > I){ period <- ((I - (DP - DCF)) / DCF) + n - 1
-      
-      # Stop as we calculated value we need
-      break } } }
   
+    DP <- DP + DCF # Payback Sum
+  
+  # When Payback Sum > Initial Value, calculate Payback Period and stop
+  if (DP > I){ period <- (I - DP) / DCF + n
+  
+    break } } }
+    
   # Put number into sentence and display value
-  return(sprintf("The payback period is %s years", round(period, 2)))
+  sprintf("The payback period is %s years", round(period, 2))
 }
 # Test
-DPR(C = cash_flow_for_dpr, r = 0.1, I = 200)
+DPR(C = c(100, 100, 100, 100), r = 0.1, I = 200)
