7bbbb253164a1fbf22f8ccd9393664e3e1a0e672
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-28 16:12:32 +0500

    Update Bond Calculator.R
    Optimised script:
    
    • Reduced excessive brackets
    • Simplified multiplication & division

--- Bond Calculators/Bond Calculator.R
+++ Bond Calculators/Bond Calculator.R
@@ -1,10 +1,10 @@
 # Bond Calculator
-B.calculator <- function(P, C, r, ytm, f = 1, exp = F){ 
+B.calculator <- function(P, C, r, y, f = 1, exp = F){ 
   
   # Exponential Valuation
-  if (isTRUE(exp)){ P*exp(-r*ytm) + sum(((C*P)/f)*exp(-r*(seq(ytm*f)/(ytm*f))))
-  # Standard Valuation
-  } else { P * (C*(1/r*(1-1/(1+(1/(f/r)))^(ytm*f))) + 1/(1+(1/(f/r)))^(ytm*f))}
+  if (isTRUE(exp)){ P * exp(-r * y) + sum(C * P/f * exp(-r * (seq(y * f)/y/f)))
+    # Standard Valuation
+  } else { P * (C * 1/r * (1 - 1/(1 + r/f) ^ (y * f)) + 1/(1 + r/f) ^ (y * f))}
 }
 # Test it
 B.calculator(1000, 0.08, 0.06, 30, 2, F)
