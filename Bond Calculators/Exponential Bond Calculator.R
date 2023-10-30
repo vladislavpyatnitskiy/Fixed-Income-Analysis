b273e52f5891f92537ad056ffd1e38d1e7ba07e6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-30 22:15:48 +0500

    Update Exponential Bond Calculator.R
    Corrected equation by reducing extra variable & moved comment higher

--- Bond Calculators/Exponential Bond Calculator.R
+++ Bond Calculators/Exponential Bond Calculator.R
@@ -1,7 +1,7 @@
 # Exponential Bond Calculator
-B.calculator.exp <- function(P, C, r, y, f = 1){
+B.calculator.exp <- function(P, C, r, y, f = 1){ # Bond Price
   
-  P * exp(-r * y) + sum(C * P/f * exp(-r * (seq(y * f)/y/f))) # Bond Value
+  P * exp(-r * y) + sum(C * P/f * exp(-r * (seq(y * f) / y)))
 }
 # Test
 B.calculator.exp(100, 0.06, 0.0676, 2, 2)
