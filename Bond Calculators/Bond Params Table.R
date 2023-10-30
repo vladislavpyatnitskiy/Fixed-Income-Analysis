39b7e3ecd4198cc4f1b23f0e15c2b49f1bbb6bda
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-30 23:07:24 +0500

    Update Bond Params Table.R
    Optimised script:
    
    • Rewrote equations
    • Renamed variables 
    • Added frequency to VaR

--- Bond Calculators/Bond Params Table.R
+++ Bond Calculators/Bond Params Table.R
@@ -1,14 +1,14 @@
 # Create Table with Bond Parameters
 B.table <- function(P, C, r, y, f = 1, s = 1, VaR = 95, PV = NULL, p = NULL){
   
-  B <- P*(C*(1/r * (1 - 1 / (1 + r/f)^(y*f))) + 1/(1 + r/f)^(y*f)) # Bond Price
+  B <- P * (C/r * (1 - (1 + r/f)^-(y * f)) + (1 + r/f)^-(y * f)) #Bond Price
   
   # Duration & Modified Duration #
-  P. <- P * (1 + C / f) / (1 + r) ^ (y * f) # Calculate part for principle
+  P. <- P * (1 + C / f) / (1 + r / f) ^ (y * f) # Principle Part
   
-  for (n in 1:(y * f - 1)){ PV <- cbind(PV, C * P / f / (1 + r) ^ n) # PV
+  for (n in 1:(y*f-1)){ PV <- cbind(PV, C * P/f/(1 + r/f)^(n * f)) # NPV
   
-    p <- cbind(p, n * PV[n]) } # Coupons
+  p <- cbind(p, n * PV[n]) } # # Coupon PV
   
   D <- (sum(p[seq(y*f - 1)]) + P.*y*f)/(P. + sum(PV[seq(y*f - 1)])) # Duration
   
@@ -24,19 +24,18 @@ B.table <- function(P, C, r, y, f = 1, s = 1, VaR = 95, PV = NULL, p = NULL){
   # Calculate all 3 prices; Product of maturity and repayment number
   for (n in 1:3){ d <- (1 + r.Cy[n] / f) ^ (y * f)
   
-    # Calculate price of bond and add result to list
-    v.Cy <- cbind(v.Cy, P * (C / r.Cy[n] * (1 - 1 / d) + 1 / d)) }
+  # Calculate price of bond and add result to list
+  v.Cy <- cbind(v.Cy, P * (C / r.Cy[n] * (1 - 1 / d) + 1 / d)) }
   
   Cy <- (v.Cy[2] + v.Cy[3] - 2 * v.Cy[1]) / (v.Cy[1] * (s * .01)^2) # Convexity
   
   # Table with all parameters #
-  l.ratios <- cbind(round(D,3),round(MD,2),round(Cy,2),B*qnorm(1-VaR*.01)*MD*r)
+  l <- cbind(round(D,3), round(MD,2), round(Cy,2), B*qnorm(1-VaR*.01)*MD*r/f)
   
   # Set column names 
-  colnames(l.ratios) <- c("Duration", "Modified Duration", "Convexity", "VaR")
+  colnames(l) <- c("Duration", "Modified Duration", "Convexity", "VaR")
   
-  # Display table
-  return(l.ratios)
+  return(l) # Display table
 }
 # Test
 B.table(P = 1000, C = 0.1, r = 0.05, y = 3, f = 1, s = 1)
