0784f3c4047540c9f24d8c1d722f0056fb1ee17d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-30 19:55:46 +0500

    Update Bond Calculator.R
    Optimised script:
    
    • Improved accuracy for exponential option
    • Reduced excessive denominators

--- Bond Calculators/Bond Calculator.R
+++ Bond Calculators/Bond Calculator.R
@@ -1,10 +1,9 @@
 # Bond Calculator
-B.calculator <- function(P, C, r, y, f = 1, exp = F){ 
+B.calculator <- function(P, C, r, y, f = 1, exp = F){ # Exponential Valuation
   
-  # Exponential Valuation
-  if (isTRUE(exp)){ P * exp(-r * y) + sum(C * P/f * exp(-r * (seq(y * f)/y/f)))
+  if (isTRUE(exp)){ P * exp(-r * y) + sum(C * P/f * exp(-r * (seq(y * f) / y)))
     # Standard Valuation
-  } else { P * (C * 1/r * (1 - 1/(1 + r/f) ^ (y * f)) + 1/(1 + r/f) ^ (y * f))}
+  } else { P * (C / r * (1 - (1 + r / f) ^ -(y * f)) + (1 + r / f) ^ -(y * f))}
 }
 # Test it
 B.calculator(1000, 0.08, 0.06, 30, 2, F)
