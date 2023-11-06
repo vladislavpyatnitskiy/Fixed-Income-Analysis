4a40645c3259628239dae7067ee2cdd452b0d394
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 15:53:55 +0500

    Update IRR & NPV graph.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Used sequence instead of list and put it into loop
    • Changed colours of curve and horizontal line
    • Renamed variables

--- Accounting measures/IRR & NPV graph.R
+++ Accounting measures/IRR & NPV graph.R
@@ -1,34 +1,17 @@
-# Project Cash Flow
-cash_flows2 <- c(-800, 300, 300, 300, 150)
-
 # Plot relationship between IRR and NPV
-npv_irr_graph <- function(x, xlim = NULL, h = 0, col, lwd = 3){
+npv.irr.plt <- function(x, xlim = NULL, h = 0, col, lwd = 3, main = NULL){ 
   
-  # Make a vector with irr's and make it matrix
-  irr_matrix <- as.matrix(seq(0, 1, by  = 0.01))
+  v <- NULL # List for NPV values
   
-  # Create 
-  values_for_npv_graph <- NULL
+  for (n in seq(0, 1, .01)){ v <- rbind(v, sum(x / (1 + n) ^ (seq(x) - 1))) }
   
-  # For each irr values
-  for (v in 1:nrow(irr_matrix)){
-    
-    # Put value in vector
-    values_for_npv_graph <- rbind(values_for_npv_graph,
-                                  sum(x / (1 + irr_matrix[v])
-                                      ^ (seq(along = x) - 1))) }
+  v.plt <- data.frame(as.matrix(seq(0, 1, .01)), v) # Join IRR and NPV
   
-  # Join NPV and IRR into one data frame
-  final_values_for_npv_graph <- data.frame(irr_matrix, values_for_npv_graph)
+  colnames(v.plt) <- c("IRR", "NPV") # Column names 
   
-  # Create column names 
-  colnames(final_values_for_npv_graph) <- c("IRR", "NPV")
+  plot(x=v.plt,type="l",xlim=xlim,main=main,las=1,col=col, lwd = lwd) # Plot 
   
-  # Plot 
-  plot(x = final_values_for_npv_graph, type = "l", xlim = xlim)
-  
-  # Make break even line more visible
-  abline(h = h, col = col, lwd = lwd)
+  abline(h = h, col = "black", lwd = 1) # Make break even line more visible
 }
-# Test
-npv_irr_graph(cash_flows2, xlim = c(0, 0.2), h = 0, col = "red", lwd = 3)
+npv.irr.plt(c(-800, 300, 300, 300, 150), xlim = c(0,1), h = 0, col = "red",
+            lwd = 3, main = "Relationship between NPV and IRR") # Test
