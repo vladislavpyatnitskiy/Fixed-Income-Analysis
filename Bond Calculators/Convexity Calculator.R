8b98a50e366b7045b282b74a26ae73ebaa887408
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 14:08:49 +0500

    Update Convexity Calculator.R
    Reduced extra step in equation

--- Bond Calculators/Convexity Calculator.R
+++ Bond Calculators/Convexity Calculator.R
@@ -3,7 +3,7 @@ Convexity <- function(P, C = 0, r = 0, y, f = 1, s = 1){ # Convexity
   l <- NULL # List for all price values
   
   # Calculate all 3 prices; Product of maturity and repayment number
-  for (n in seq(-1, 1, 1)){ d <- (1 + 1 / f * (r + s * n / 100)) ^ (y * f)
+  for (n in seq(-1, 1, 1)){ d <- (1 + (r + s * n / 100) / f) ^ (y * f)
   
     # Calculate price of bond and add result to list
     l <- cbind(l, P * (C / (r + s * n / 100) * (1 - 1 / d) + 1 / d)) }
