4987a694ef88e4a493f4f6dbe6aaf3cfead9ff7f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 14:37:06 +0500

    Update Bond Params Table.R
    Optimised script:
    
    • Improved Convexity part by using sequence instead of list
    • Substituted *.01 by /100 to improve script perception

--- Bond Calculators/Bond Params Table.R
+++ Bond Calculators/Bond Params Table.R
@@ -1,41 +1,37 @@
 # Create Table with Bond Parameters
 B.table <- function(P, C, r, y, f = 1, s = 1, VaR = 95, PV = NULL, p = NULL){
   
-  B <- P * (C/r * (1 - (1 + r/f)^-(y * f)) + (1 + r/f)^-(y * f)) #Bond Price
+  B <- P * (C/r * (1 - (1 + r/f)^-(y * f)) + (1 + r/f)^-(y * f)) # Bond Price
   
   # Duration & Modified Duration #
   P. <- P * (1 + C / f) / (1 + r / f) ^ (y * f) # Principle Part
   
-  for (n in 1:(y*f-1)){ PV <- cbind(PV, C * P/f/(1 + r/f)^(n * f)) # NPV
+  for (n in 1:(y*f-1)){ PV <- cbind(PV, C * P / f / (1 + r / f)^(n * f)) # NPV
   
-  p <- cbind(p, n * PV[n]) } # # Coupon PV
+    p <- cbind(p, n * PV[n]) } # # Coupon PV
   
   D <- (sum(p[seq(y*f - 1)]) + P.*y*f)/(P. + sum(PV[seq(y*f - 1)])) # Duration
   
-  MD <- D / (1 + (r - s * .01) / f) # Modified Duration
+  MD <- D / (1 + (r - s / 100) / f) # Modified Duration
   
   # Convexity #
   
-  # Put all interest rate values into one vector
-  r.Cy <- c(r / f, (r - s * 0.01) / f, (r + s * 0.01) / f)
-  
-  v.Cy <- NULL # Create an empty list to contain all price values
+  v <- NULL # List for all price values
   
   # Calculate all 3 prices; Product of maturity and repayment number
-  for (n in 1:3){ d <- (1 + r.Cy[n] / f) ^ (y * f)
+  for (n in seq(-1, 1, 1)){ d <- (1 + (r + s * n / 100) / f) ^ (y * f)
   
-  # Calculate price of bond and add result to list
-  v.Cy <- cbind(v.Cy, P * (C / r.Cy[n] * (1 - 1 / d) + 1 / d)) }
+    # Calculate price of bond and add result to list
+    v <- cbind(v, P * (C / (r + s * n / 100) * (1 - 1 / d) + 1 / d)) }
   
-  Cy <- (v.Cy[2] + v.Cy[3] - 2 * v.Cy[1]) / (v.Cy[1] * (s * .01)^2) # Convexity
+  Cy <- (v[1] + v[3] - 2 * v[2]) / v[2] / (s / 100) ^ 2 # Convexity
   
   # Table with all parameters #
-  l <- cbind(round(D,3), round(MD,2), round(Cy,2), B*qnorm(1-VaR*.01)*MD*r/f)
+  l <- cbind(round(D,3), round(MD,2), round(Cy,2), B*qnorm(1 - VaR/100)*MD*r/f)
   
-  # Set column names 
+  # Column names 
   colnames(l) <- c("Duration", "Modified Duration", "Convexity", "VaR")
   
   return(l) # Display table
 }
-# Test
-B.table(P = 1000, C = 0.1, r = 0.05, y = 3, f = 1, s = 1)
+B.table(P = 1000, C = 0.1, r = 0.05, y = 3, f = 1, s = 1) # Test
