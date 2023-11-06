802826c92c5d2becd9ab92697c3e43bf44c0a3b3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 14:00:39 +0500

    Update Convexity Calculator.R
    Optimised & Upgraded & Debugged script:
    
    • Substitute list with sequence integrated into loop
    • Rewrote equation to eliminate excessive steps
    • Renamed some variables for better code perception
    • Changed some values, like .01 to 100 for better code perception
    • Edited comments

--- Bond Calculators/Convexity Calculator.R
+++ Bond Calculators/Convexity Calculator.R
@@ -1,18 +1,13 @@
-# Convexity
-Convexity <- function(P, C = 0, r = 0, ytm, f = 1, s = 1){
+Convexity <- function(P, C = 0, r = 0, y, f = 1, s = 1){ # Convexity
   
-  r.Cy <- c(r, r - s * 0.01, r + s * 0.01) # Put all rates into one vector
-  
-  v.Cy <- NULL # Create an empty list to contain all price values
+  l <- NULL # List for all price values
   
   # Calculate all 3 prices; Product of maturity and repayment number
-  for (n in 1:3){ d <- (1 + 1 / (f / r.Cy[n])) ^ (ytm * f)
+  for (n in seq(-1, 1, 1)){ d <- (1 + 1 / f * (r + s * n / 100)) ^ (y * f)
   
     # Calculate price of bond and add result to list
-    v.Cy <- cbind(v.Cy, P * (C * (1 / r.Cy[n] * (1 - 1 / d)) + 1 / d)) }
+    l <- cbind(l, P * (C / (r + s * n / 100) * (1 - 1 / d) + 1 / d)) }
   
-  # Convexity
-  return(v.Cy[2] + v.Cy[3] - 2 * v.Cy[1]) / (v.Cy[1] * (s * 0.01) ^ 2)
+  (l[1] + l[3] - 2 * l[2]) / l[2] / (s / 100) ^ 2 # Convexity
 }
-# Test
-Convexity(P = 1000, C = 0.1, r = 0.05, ytm = 3, f = 1, s = 1)
+Convexity(P = 1000, C = 0.1, r = 0.05, y = 3, f = 1, s = 1) # Test
