ea3022b7272c2dfa2c366242408ce2af1ba13788
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 16:03:14 +0500

    Update MIRR.R
    Optimised script:
    
    • Combined round operation into MIRR calculation
    • Eliminated print() as extra step
    • Put cash flow array instead of variable containing cash flow array

--- Accounting measures/MIRR.R
+++ Accounting measures/MIRR.R
@@ -1,20 +1,9 @@
-# First project
-cash_flow_for_mirr1 <- c(200, 400, 350, 300) 
-
-# Second Project
-cash_flow_for_mirr2 <- c(300, 300, 300, 300) # y = 0.12 cf_0 = 800
-
 # MIRR
-MIRR <- function(C, r = 0.1, I = 1000){
+MIRR <- function(C, r = 0.1, I = 1000){ # Calculation & Recommendation
   
-  # MIRR
-  mirr <- (sum(C * ((1 + r) ^ (length(C) - seq(C)))) / I) ^ (1 / length(C)) - 1
+  mirr <- round((sum(C*((1+r)^(length(C)-seq(C))))/I)^(1/length(C))-1, 4) * 100
   
-  # Recommendation
-  if (mirr > r){ print(sprintf("MIRR is %s%% > cost of capital. Start project",
-                               round(mirr, 4) * 100)) 
-  } else { print(sprintf("MIRR is %s%% < cost of capital. Don't start",
-                           round(mirr, 4) * 100)) }
+  if (mirr > r){ sprintf("MIRR is %s%% > cost of capital. Start project", mirr)
+  } else { sprintf("MIRR is %s%% < cost of capital. Don't start", mirr) }
 }
-# Test
-MIRR(C = cash_flow_for_mirr2, r = 0.12, I = 800)
+MIRR(C = c(300, 300, 300, 300), r = 0.12, I = 800) # Test
