ca21c00bfb5f9899b684594dd47e4ca75c2df7d6
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-06 13:10:36 +0500

    Create Yield to Bond Price Plot.R
    Added script to generate plot showing relationship between bond yield and price

---
+++ Bond Plots/Yield to Bond Price Plot.R
@@ -0,0 +1,30 @@
+# Relationship between rate and price
+bond.y.plt <- function(P, C = 0, y, f = 1, l.bound = 0, h.bound = 100){
+  
+  l.B.prices <- NULL # Empty lists for prices
+  
+  # For each yield value add bond price value to list
+  for (n in l.bound:h.bound){ d <- (1 + .01 * n / f) ^ -y * f
+  
+    l.B.prices <- c(l.B.prices, P * (C / .01 / n * (1 - d) + d)) }
+  
+  # Generate plot
+  plot(x = as.vector(seq(l.bound, h.bound) * .01),
+       y = l.B.prices,
+       type = "l",
+       xlab = "Bond Yield",
+       ylab = "Bond Price",
+       main = "Bond Pricing Dependency On Yield",
+       col = "red",
+       lwd = 3,
+       las = 1)
+  
+  # Axes 
+  axis(side = 1, at = seq(10, 90, 20) * .01)
+  axis(side = 2, at = seq(200, 1200, 100), las = 1)
+  
+  # Dotted lines
+  abline(v = seq(-1, 1, .1), lty = 3, col = "grey") 
+  abline(h = seq(0, 1500, 100), lty = 3, col = "grey")
+}
+bond.y.plt(P = 1000, C = .08, y = 3, f = 1, l.bound = 0, h.bound = 100) # Test
