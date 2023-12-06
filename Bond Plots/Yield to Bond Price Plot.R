3ef630dc30dad29a2d6d64bf0f318fee6146a43f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-12-06 13:23:19 +0500

    Update Yield to Bond Price Plot.R
    Fixed bugs with scales and updated format

--- Bond Plots/Yield to Bond Price Plot.R
+++ Bond Plots/Yield to Bond Price Plot.R
@@ -9,10 +9,10 @@ bond.y.plt <- function(P, C = 0, y, f = 1, l.bound = 0, h.bound = 100){
     l.B.prices <- c(l.B.prices, P * (C / .01 / n * (1 - d) + d)) }
   
   # Generate plot
-  plot(x = as.vector(seq(l.bound, h.bound) * .01),
+  plot(x = as.vector(seq(l.bound, h.bound)),
        y = l.B.prices,
        type = "l",
-       xlab = "Bond Yield",
+       xlab = "Bond Yield (%)",
        ylab = "Bond Price",
        main = "Bond Pricing Dependency On Yield",
        col = "red",
@@ -20,11 +20,11 @@ bond.y.plt <- function(P, C = 0, y, f = 1, l.bound = 0, h.bound = 100){
        las = 1)
   
   # Axes 
-  axis(side = 1, at = seq(10, 90, 20) * .01)
-  axis(side = 2, at = seq(200, 1200, 100), las = 1)
+  axis(side = 1, at = seq(0, 100, 5))
+  axis(side = 2, at = seq(100, 9000, 100), las = 1)
   
   # Dotted lines
-  abline(v = seq(-1, 1, .1), lty = 3, col = "grey") 
-  abline(h = seq(0, 1500, 100), lty = 3, col = "grey")
+  abline(v = seq(0, 100, 5), lty = 3, col = "grey") 
+  abline(h = seq(0, 9000, 100), lty = 3, col = "grey")
 }
-bond.y.plt(P = 1000, C = .08, y = 3, f = 1, l.bound = 0, h.bound = 100) # Test
+bond.y.plt(P = 1000, C = .1, y = 10, f = 1, l.bound = 0, h.bound = 100) # Test
