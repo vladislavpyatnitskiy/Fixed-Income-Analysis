e200126a4388957e9c57b5902f11cdfd5c27c188
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-13 17:05:30 +0500

    Update IRR & NPV graph.R
    Optimised script:
    
    • Combined data frame creation and column names assignation and integrated them into plot generation function

--- Accounting measures/IRR & NPV graph.R
+++ Accounting measures/IRR & NPV graph.R
@@ -5,11 +5,8 @@ npv.irr.plt <- function(x, xlim = NULL, h = 0, col, lwd = 3, main = NULL){
   
   for (n in seq(0, 1, .01)){ v <- rbind(v, sum(x / (1 + n) ^ (seq(x) - 1))) }
   
-  v.plt <- data.frame(as.matrix(seq(0, 1, .01)), v) # Join IRR and NPV
-  
-  colnames(v.plt) <- c("IRR", "NPV") # Column names 
-  
-  plot(x=v.plt,type="l",xlim=xlim,main=main,las=1,col=col, lwd = lwd) # Plot 
+  plot(x = data.frame(as.matrix(seq(0, 1, .01)),v), xlab = "IRR", ylab = "NPV",
+       type="l", xlim=xlim, main = main, las = 1, col = col, lwd = lwd) # Plot 
   
   abline(h = h, col = "black", lwd = 1) # Make break even line more visible
 }
