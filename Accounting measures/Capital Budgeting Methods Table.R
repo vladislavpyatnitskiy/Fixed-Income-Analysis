45aea86ca12689369fa464f83a8eea84d1f766ba
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-13 16:56:34 +0500

    Update Capital Budgeting Methods Table.R
    Upgraded script:
    
    • Added option to get Capital Budgeting Methods values for multiple cash flows/projects
    • Added option to show project names in place of row names

--- Accounting measures/Capital Budgeting Methods Table.R
+++ Accounting measures/Capital Budgeting Methods Table.R
@@ -1,26 +1,41 @@
 # Table with capital budgeting methods
-cash.flow.rates <- function(C, r){ npv <- sum(C/(1 + r)^(seq(C) - 1)) # NPV
-
-  irr <- uniroot(function(C, r) (sum(C/(1+r)^(seq(C)-1))), c(0, 1), C = C)$root
+cash.flow.rates <- function(CF, rt, project = NULL){ df.ratios <- NULL
   
-  DP <- 0 # Sum of paybacks
-  period <- 0 # Payback Period
-  I <- -C[1] # Initial Investment
-  C1 <- C[2:length(C)] # Cash flow vector for payback and mirr
+  for (m in 1:ncol(CF)){ C <- CF[,m] #
+    
+    r <- rt[m] #
   
-  # Until sum of payback < initial value (IV)
-  while (DP <= I){ for (n in 1:length(C1)){ DP <- DP + C1[n]/(1 + r) ^ n
+    npv <- sum(C/(1 + r)^(seq(C) - 1)) # NPV
   
-  if (DP > I){ period <- (I - DP) / (C1[n] / (1 + r) ^ n) + n # Payback > IV
+    irr <- uniroot(function(C,r) (sum(C/(1+r)^(seq(C)-1))), c(0,1), C=C)$root
+    
+    DP <- 0 # Sum of paybacks
+    period <- 0 # Payback Period
+    I <- -C[1] # Initial Investment
+    C1 <- C[2:length(C)] # Cash flow vector for payback and mirr
+    
+    # Until sum of payback < initial value (IV)
+    while (DP <= I){ for (n in 1:length(C1)){ DP <- DP + C1[n]/(1 + r) ^ n
+    
+    if (DP > I){ period <- (I - DP) / (C1[n] / (1 + r) ^ n) + n # Payback > IV
+    
+    break } } } # End
+    
+    mirr <- (sum(C1*((1+r)^(length(C1)-seq(C1))))/I)^(1/length(C1))-1 # MIRR
+    
+    df.ratios <- rbind(df.ratios, cbind(npv, irr, period, mirr)) } 
+    
+  colnames(df.ratios) <- c("NPV", "IRR", "Payback", "MIRR") # Set column names 
   
-  break } } } # End
+  if (is.null(project)){ rownames(df.ratios) <- seq(ncol(CF)) } else {
+    
+    rownames(df.ratios) <- project } # Project names when typed
   
-  mirr <- (sum(C1*((1 + r)^(length(C1) - seq(C1))))/I)^(1/length(C1))-1 # MIRR
-  
-  a.ratios <- cbind(npv, irr, period, mirr) # Put values into list
-  
-  colnames(a.ratios) <- c("NPV", "IRR", "Payback", "MIRR") # Set column names 
-  
-  return(a.ratios) # Display sentence
+  return(df.ratios) # Display sentence
 }
-cash.flow.rates(C = c(-800, 300, 300, 300, 300), r = 0.12) # Test
+cash.flow.rates(CF = cbind(c(-800, 300, 300, 300, 300),
+                            c(-1000, 250, 300, 360, 432),
+                            c(-200, 100, 100, 100, 100),
+                            c(-800, 300, 300, 300, 150)),
+                 rt = c(0.12, 0.05, 0.1, 0.1),
+                 project = c("Blue", "Red", "Green", "Yellow")) # Test
