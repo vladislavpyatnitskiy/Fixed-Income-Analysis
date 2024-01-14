a4d3669d3a28985eee807786330e123570f3d5ac
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-14 12:09:09 +0500

    Update IRR & NPV graph.R
    Upgraded script:
    
    • Added option to generate plots for multiple cash flows simultaneously

--- Accounting measures/IRR & NPV graph.R
+++ Accounting measures/IRR & NPV graph.R
@@ -1,14 +1,30 @@
 # Plot relationship between IRR and NPV
 npv.irr.plt <- function(x, xlim = NULL, h = 0, col, lwd = 3, main = NULL){ 
   
-  v <- NULL # List for NPV values
+  if (is.array(x)) { l <- NULL # Scenario for Multiple Cash Flows
   
-  for (n in seq(0, 1, .01)){ v <- rbind(v, sum(x / (1 + n) ^ (seq(x) - 1))) }
-  
-  plot(x = data.frame(as.matrix(seq(0, 1, .01)),v), xlab = "IRR", ylab = "NPV",
-       type="l", xlim=xlim, main = main, las = 1, col = col, lwd = lwd) # Plot 
-  
-  abline(h = h, col = "black", lwd = 1) # Make break even line more visible
+    for (m in 1:ncol(x)){ s <- x[,m]
+    
+      v <- NULL # List for NPV values
+      
+      for (n in seq(0, 1, .01)){ v <- rbind(v, sum(s / (1 + n)^(seq(s) - 1))) }
+      
+      plt <-  plot(x = data.frame(as.matrix(seq(0, 1, .01)), v), xlab = "IRR",
+                   ylab = "NPV", type = "l", xlim = xlim, main = main, las = 1,
+                   col = col, lwd = lwd) # Plot 
+      
+      abline(h = h, col = "black", lwd = 1) # Break Even horizontal line
+      
+      l <- list(l, plt) } } else { v <- NULL # Scenario for one cash flow
+    
+      for (n in seq(0, 1, .01)){ v <- rbind(v, sum(x / (1 + n)^(seq(x) - 1))) }
+    
+      plt <-  plot(x = data.frame(as.matrix(seq(0, 1, .01)),v), xlab = "IRR",
+                   ylab = "NPV", type = "l", xlim = xlim, main = main, las = 1,
+                   col = col, lwd = lwd) # Plot 
+      
+      abline(h = h, col = "black", lwd = 1) }
 }
-npv.irr.plt(c(-800, 300, 300, 300, 150), xlim = c(0,1), h = 0, col = "red",
-            lwd = 3, main = "Relationship between NPV and IRR") # Test
+npv.irr.plt(x=cbind(c(-1000, 250, 300, 360, 432), c(-800, 300, 300, 300, 150)),
+            xlim = c(0, 1), h = 0, col = "red", lwd = 3,
+            main="Relationship between NPV and IRR") # Test
